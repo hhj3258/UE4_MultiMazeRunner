e0e72819c90c4a5ff6563674a02688f864f202a0
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-04-28 20:40:54 +0900

    Multi Maze Runner Upload
    Multi Maze Runner Upload

---
+++ Project_MultiMazeRunner/Source/My3/My3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "My3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, My3, "My3" );
---
+++ Project_MultiMazeRunner/Source/My3Server.Target.cs
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+[SupportedPlatforms(UnrealPlatformClass.Server)]
+public class My3ServerTarget : TargetRules
+{
+    public My3ServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		BuildEnvironment = TargetBuildEnvironment.Shared;
+		ExtraModuleNames.Add("My3");
+	}
+}
